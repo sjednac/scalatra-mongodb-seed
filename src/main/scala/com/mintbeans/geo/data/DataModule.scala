dd1002b66e753c4887b8b20200f6a8142ff70445
Szymon Biliński <szymon.bilinski@gmail.com>
2015-03-07 13:45:44 +0100

    Mongo client initialisation info

--- src/main/scala/com/mintbeans/geo/data/DataModule.scala
+++ src/main/scala/com/mintbeans/geo/data/DataModule.scala
@@ -4,16 +4,25 @@ import com.mintbeans.geo.core.LocationRepository
 import com.mongodb.casbah.MongoClient
 import com.softwaremill.macwire.MacwireMacros._
 import com.typesafe.config.Config
+import org.slf4j.LoggerFactory
 
 trait DataModule {
 
   val config: Config
 
-  lazy val mongo = MongoClient(config.getString("mongo.host"), config.getInt("mongo.port"))
+  lazy val mongo = {
+    val host = config.getString("mongo.host")
+    val port = config.getInt("mongo.port")
+
+    val logger = LoggerFactory.getLogger(classOf[DataModule])
+    logger.info("Mongo client: host={} port={}", host, port)
+
+    MongoClient(host, port)
+  }
+
   lazy val db = mongo(config.getString("mongo.db"))
 
   lazy val locationCollection = db(config.getString("mongo.collections.locations"))
   lazy val locationRepository: LocationRepository = wire[MongoLocationRepository]
-
 
 }
